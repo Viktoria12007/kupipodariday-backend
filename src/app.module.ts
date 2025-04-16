eec245f5dffd8dc63545aac750d8298ecbc72948
Виктория Голубова <v.golubova@denexy.com>
2025-04-16 15:45:11 +0600

    Generated offers entity.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { WishesModule } from './wishes/wishes.module';
 import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { WishlistsModule } from './wishlists/wishlists.module';
     UsersModule,
     WishesModule,
     WishlistsModule,
+    OffersModule,
   ],
   controllers: [AppController],
   providers: [],
