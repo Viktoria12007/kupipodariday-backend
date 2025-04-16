4d9c64c04ad5367c0ef99cd9f14a7f7bcd4b2cf8
Виктория Голубова <v.golubova@denexy.com>
2025-04-16 15:42:48 +0600

    Generated wishes entity.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UsersModule } from './users/users.module';
       synchronize: true,
     }),
     UsersModule,
+    WishesModule,
   ],
   controllers: [AppController],
   providers: [],
