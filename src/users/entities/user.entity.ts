be0f78566954cea9b34443d605ff78c0a64f6e7c
Виктория Голубова <v.golubova@denexy.com>
2025-04-17 11:57:03 +0600

    The user scheme is described.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,66 @@
-export class User {}
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    OneToMany, OneToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
+import { IsDefined, IsEmail, IsString, IsUrl, Length } from "class-validator";
+import {Wish} from "../../wishes/entities/wish.entity";
+import {Wishlist} from "../../wishlists/entities/wishlist.entity";
+import {Offer} from "../../offers/entities/offer.entity";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    @OneToOne(() => Offer)
+    @JoinColumn()
+    id: number;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @Column({
+        unique: true,
+    })
+    @IsString()
+    @Length(2, 30)
+    @IsDefined()
+    username: string;
+
+    @Column({
+        default: 'Пока ничего не рассказал о себе'
+    })
+    @IsString()
+    @Length(2, 200)
+    about: string;
+
+    @Column({
+        default: 'https://i.pravatar.cc/300'
+    })
+    @IsUrl()
+    avatar: string;
+
+    @Column({
+        unique: true,
+    })
+    @IsEmail()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @OneToMany(() => Wish, wish => wish.owner)
+    wishes: Wish[];
+
+    @OneToMany(() => Wish, wish => wish.offers)
+    offers: Wish[];
+
+    @OneToMany(() => Wishlist, wishlist => wishlist.id)
+    wishlist: Wishlist[];
+}
