dbfecee21601679aa39e951afb35ac4d3c5b474a
Виктория Голубова <v.golubova@denexy.com>
2025-04-17 11:58:26 +0600

    The wishlist scheme is described.

--- src/wishlists/entities/wishlist.entity.ts
+++ src/wishlists/entities/wishlist.entity.ts
@@ -1,1 +1,33 @@
-export class Wishlist {}
+import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+import {IsString, IsUrl, Length} from "class-validator";
+import {Wish} from "../../wishes/entities/wish.entity";
+
+@Entity()
+export class Wishlist {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @Column()
+    @IsString()
+    @Length(1, 250)
+    name: string;
+
+    @Column()
+    @IsString()
+    @Length(0, 1500)
+    description: string;
+
+    @Column()
+    @IsString()
+    @IsUrl()
+    image: string;
+
+    @OneToMany(() => Wish, wish => wish.id)
+    items: Wish[];
+}
