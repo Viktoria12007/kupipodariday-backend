e4d13f68d9cab63f47ac3cc7f59d31b77a3b2ad8
Виктория Голубова <v.golubova@denexy.com>
2025-04-17 11:57:56 +0600

    The offer scheme is described.

--- src/offers/entities/offer.entity.ts
+++ src/offers/entities/offer.entity.ts
@@ -1,1 +1,35 @@
-export class Offer {}
+import {Column, CreateDateColumn, Entity, ManyToOne, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+import {IsNumber} from "class-validator";
+import {User} from "../../users/entities/user.entity";
+import {Wish} from "../../wishes/entities/wish.entity";
+
+@Entity()
+export class Offer {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @OneToOne(() => User, user => user.id)
+    user: User;
+
+    @ManyToOne(() => Wish, wish => wish.offers)
+    item: Wish;
+
+    @Column({
+        type: "money",
+    })
+    @IsNumber({
+        maxDecimalPlaces: 2,
+    })
+    amount: number;
+
+    @Column({
+        default: false,
+    })
+    hidden: boolean;
+}
