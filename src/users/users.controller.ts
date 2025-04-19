b372353ebe314e09e5ea9e55af9fd7bdee72ba35
Виктория Голубова <v.golubova@denexy.com>
2025-04-19 21:01:21 +0600

    Added use of JwtGuard.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Req
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtGuard } from '../guards/jwt.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtGuard)
+  @Get()
+  profile(@Req() req) {
+    const user = req.user;
+
+    return `Logged in as ${user.username}`;
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -13,8 +32,8 @@ export class UsersController {
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  findFew(@Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.findFew(updateUserDto);
   }
 
   @Get(':id')
@@ -24,11 +43,11 @@ export class UsersController {
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    return this.usersService.updateOne(+id, updateUserDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.removeOne(+id);
   }
 }
