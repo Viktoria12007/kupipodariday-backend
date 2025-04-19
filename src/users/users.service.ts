f05a0869a3e19ec8b3c083987ee1096194a4bbbc
Виктория Голубова <v.golubova@denexy.com>
2025-04-19 21:00:02 +0600

    Added method 'findByUsername'.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,15 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import {InjectRepository} from "@nestjs/typeorm";
-import {User} from "./entities/user.entity";
-import {Repository} from "typeorm";
+import { User } from "./entities/user.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
 
   create(createUserDto: CreateUserDto) {
+    // const user = await this.usersRepository.create(createUserDto);
+    // return this.usersRepository.save(user);
+
     return this.userRepository.save(createUserDto);
+  }
+
+  async findByUsername(username: string) {
+    const user = await this.userRepository.findOne({ username });
+
+    return user;
   }
 
   findFew(updateUserDto) {
