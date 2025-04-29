13d02a4562dcc0338461c984c5c154844a517088
Виктория Голубова <v.golubova@denexy.com>
2025-04-29 19:29:23 +0600

    Adjusting users.controller.ts methods. Added decorator '@UseGuards'.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,53 +1,44 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-  Req
-} from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { JwtGuard } from '../guards/jwt.guard';
+import { Body, Controller, Get, Param, Patch, Post, Req, UseGuards } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { JwtGuard } from "../guards/jwt.guard";
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @UseGuards(JwtGuard)
-  @Get()
+  @Get('me')
   profile(@Req() req) {
-    const user = req.user;
-
-    return `Logged in as ${user.username}`;
+    return this.usersService.findOne(req.user.id);
   }
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @UseGuards(JwtGuard)
+  @Patch('me')
+  updateProfile(@Req() req, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.updateOne(req.user.id, updateUserDto);
   }
 
-  @Get()
-  findFew(@Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.findFew(updateUserDto);
+  @UseGuards(JwtGuard)
+  @Get('me/wishes')
+  async meWishes(@Req() req) {
+    const me = await this.usersService.findOne(req.user.id);
+    return me.wishes;
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @Get(':username')
+  findByUsername(@Param('username') username: string ) {
+    return this.usersService.findOne({ username });
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.updateOne(+id, updateUserDto);
+  @Get(':username/wishes')
+  async findWishesByUsername(@Param('username') username: string ) {
+    const user = await this.usersService.findOne({ username });
+    return user.wishes;
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.removeOne(+id);
+  @Post('find')
+  findByUsernameOrEmail(@Body() body: { query: string }) {
+    return this.usersService.findByUsernameOrEmail(body.query);
   }
 }
