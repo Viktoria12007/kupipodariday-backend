ee6b44735f604f9404d10c9cc02c91a6e04bca35
Виктория Голубова <v.golubova@denexy.com>
2025-05-06 12:57:29 +0600

    Automatic documentation has been set up for controller 'user'.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,31 +1,56 @@
-import { Body, Controller, Get, Param, Patch, Post, Req, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, Param, Patch, Post, Req, UseGuards } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { JwtGuard } from "../guards/jwt.guard";
+import {
+  ApiBearerAuth,
+  ApiExtraModels,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+  OmitType
+} from "@nestjs/swagger";
+import { User } from "./entities/user.entity";
+import { NoValidUserResponse } from "./dto/no-valid-user-response";
+import { FindUserDto } from "./dto/find-user.dto";
 
+@ApiBearerAuth()
+@ApiTags('users')
+@ApiExtraModels(User)
 @Controller('users')
+@UseGuards(JwtGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(JwtGuard)
+  @ApiResponse({
+    description: 'Возвращает пользователя по токену',
+    status: 200,
+    type: OmitType(User, ['password', 'email'])
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized', type: NoValidUserResponse })
   @Get('me')
   profile(@Req() req) {
     return this.usersService.findOne({ where: { id: req.user.id }});
   }
 
-  @UseGuards(JwtGuard)
   @Patch('me')
-  updateProfile(@Req() req, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.updateOne({ id: req.user.id }, updateUserDto);
+  async updateProfile(@Req() req, @Body() updateUserDto: UpdateUserDto) {
+    const { password, ...user} = await this.usersService.updateOne({ where: { id: req.user.id }}, updateUserDto);
+    return user;
   }
 
-  @UseGuards(JwtGuard)
   @Get('me/wishes')
   async meWishes(@Req() req) {
     const me = await this.usersService.findOne({ where: { id: req.user.id }});
     return me.wishes;
   }
 
+  @ApiParam({
+    name: 'username',
+    description: 'Username of the user',
+    example: 'Test user 1'
+  })
   @Get(':username')
   findByUsername(@Param('username') username: string ) {
     return this.usersService.findOne({ where: { username } });
@@ -38,7 +63,9 @@ export class UsersController {
   }
 
   @Post('find')
-  findByUsernameOrEmail(@Body() body: { query: string }) {
-    return this.usersService.findMany({ where: [{ username: body.query}, { email: body.query} ] });
+  @HttpCode(200)
+  findByUsernameOrEmail(@Body() findUserDto: FindUserDto) {
+    const { email, username } = findUserDto;
+    return this.usersService.findMany({ where: [{ username }, { email }] });
   }
 }
