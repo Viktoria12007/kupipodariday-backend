0b14576ce6ffa9464be7fa7ddcdbe56e22a7455e
Виктория Голубова <v.golubova@denexy.com>
2025-05-06 12:55:24 +0600

    Automatic documentation has been set up for entity 'user'.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,55 +1,60 @@
-import {
-    Column,
-    CreateDateColumn,
-    Entity,
-    OneToMany,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn
-} from "typeorm";
-import { IsDefined, IsEmail, IsString, IsUrl, Length } from "class-validator";
-import {Wish} from "../../wishes/entities/wish.entity";
-import {Wishlist} from "../../wishlists/entities/wishlist.entity";
-import {Offer} from "../../offers/entities/offer.entity";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { IsEmail, IsOptional, IsString, IsUrl, Length } from "class-validator";
+import { Wish } from "../../wishes/entities/wish.entity";
+import { Wishlist } from "../../wishlists/entities/wishlist.entity";
+import { Offer } from "../../offers/entities/offer.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
 export class User {
+    @ApiProperty({ description: 'id пользователя'})
     @PrimaryGeneratedColumn()
     id: number;
 
+    @ApiProperty({ description: 'дата создания пользователя'})
     @CreateDateColumn()
     createdAt: Date;
 
+    @ApiProperty({ description: 'дата обновления пользователя'})
     @UpdateDateColumn()
     updatedAt: Date;
 
+    @ApiProperty({ description: 'Имя пользователя', example: 'Test user 1' })
+    @IsString()
+    @Length(2, 30)
     @Column({
         unique: true,
     })
-    @IsString()
-    @Length(2, 30)
-    @IsDefined()
     username: string;
 
+    @ApiProperty({ description: 'Описание пользователя', example: 'About test user 1' })
+    @IsString()
+    @Length(2, 200)
+    @IsOptional()
     @Column({
         default: 'Пока ничего не рассказал о себе'
     })
-    @IsString()
-    @Length(2, 200)
     about: string;
 
+    @ApiProperty({ description: 'Аватар пользователя', example: 'https://i.pravatar.cc/300' })
+    @IsUrl()
+    @IsOptional()
     @Column({
         default: 'https://i.pravatar.cc/300'
     })
-    @IsUrl()
     avatar: string;
 
+    @ApiProperty({ description: 'Email пользователя', example: 'admin@gmail.com' })
+    @IsEmail()
     @Column({
         unique: true,
+        select: false,
     })
-    @IsEmail()
     email: string;
 
-    @Column()
+    @ApiProperty({ description: 'Пароль пользователя', example: '123456789' })
+    @IsString()
+    @Column({ select: false })
     password: string;
 
     @OneToMany(() => Wish, wish => wish.owner)
