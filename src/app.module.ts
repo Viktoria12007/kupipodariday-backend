d69b4309bd651b017c37951aca0f4f774fbb387b
Виктория Голубова <v.golubova@denexy.com>
2025-05-07 09:50:25 +0600

    Changed options for ConfigModule and TypeOrmModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,15 +11,15 @@ import { ConfigModule } from "@nestjs/config";
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      isGlobal: true,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'student',
-      password: 'student',
-      database: 'nest_project',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       autoLoadEntities: true,
       entities: [__dirname + '/**/*.entity.ts'],
       synchronize: true,
