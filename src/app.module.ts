dbb484e70a66a79f9eef637d139992e377bf29c8
Виктория Голубова <v.golubova@denexy.com>
2025-05-08 13:16:09 +0600

    Added using DatabaseConfigFactory.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,9 @@ import { WishesModule } from './wishes/wishes.module';
 import { WishlistsModule } from './wishlists/wishlists.module';
 import { OffersModule } from './offers/offers.module';
 import { AuthModule } from "./auth/auth.module";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 import configuration from "./config/configuration";
+import { DatabaseConfigFactory } from "./config/database-config.factory";
 
 @Module({
   imports: [
@@ -16,19 +17,7 @@ import configuration from "./config/configuration";
       load: [configuration],
     }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('database.host'),
-        port: configService.get<number>('database.port'),
-        username: configService.get<string>('database.user'),
-        password: configService.get<string>('database.password'),
-        database: configService.get<string>('database.name'),
-        autoLoadEntities: true,
-        entities: [__dirname + '/**/*.entity.ts'],
-        synchronize: true,
-      })
+      useClass: DatabaseConfigFactory,
     }),
     AuthModule,
     UsersModule,
