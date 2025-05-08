c51f87e2975822adf1086e10d70c29d9fc937281
Виктория Голубова <v.golubova@denexy.com>
2025-05-08 13:12:46 +0600

    Created database-config.factory.ts

---
+++ src/config/database-config.factory.ts
@@ -0,0 +1,22 @@
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+import { ConfigService } from "@nestjs/config";
+import { join } from 'path';
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class DatabaseConfigFactory implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('database.host'),
+      port: this.configService.get<number>('database.port'),
+      username: this.configService.get<string>('database.user'),
+      password: this.configService.get<string>('database.password'),
+      database: this.configService.get<string>('database.name'),
+      autoLoadEntities: true,
+      entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
+      synchronize: true,
+    }
+  }
+}
