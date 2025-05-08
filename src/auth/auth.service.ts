09f64ef6dc557cd14abf90949c123ad6bbfe4509
Виктория Голубова <v.golubova@denexy.com>
2025-05-08 13:33:07 +0600

    Added option 'select' and call 'verifyHash'.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { User } from "../users/entities/user.entity";
 import { JwtService } from "@nestjs/jwt";
 import { UsersService } from "../users/users.service";
+import { verifyHash } from "../helpers/hash";
 
 @Injectable()
 export class AuthService {
@@ -14,9 +15,16 @@ export class AuthService {
     }
 
     async validatePassword(username: string, password: string) {
-        const user = await this.usersService.findOne({ where: { username } });
+        const user = await this.usersService.findOne({
+            where: { username },
+            select: {
+                id: true,
+                username: true,
+                password: true,
+            }
+        });
 
-        if (user && user.password === password) {
+        if (user && (await verifyHash(password, user.password))) {
             const { password, ...result } = user;
             return result;
         }
