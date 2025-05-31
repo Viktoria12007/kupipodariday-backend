9083a4ce912e4aa369bcf2c71c95784c658afa88
Виктория Голубова <v.golubova@denexy.com>
2025-05-31 19:41:42 +0600

    Corrected method 'create' for wishlists.service.ts

--- src/wishlists/wishlists.service.ts
+++ src/wishlists/wishlists.service.ts
@@ -3,14 +3,19 @@ import { CreateWishlistDto } from './dto/create-wishlist.dto';
 import { UpdateWishlistDto } from './dto/update-wishlist.dto';
 import {InjectRepository} from "@nestjs/typeorm";
 import { FindManyOptions, FindOneOptions, FindOptionsWhere, Repository } from "typeorm";
-import {Wishlist} from "./entities/wishlist.entity";
+import { Wishlist } from "./entities/wishlist.entity";
+import { UsersService } from "../users/users.service";
 
 @Injectable()
 export class WishlistsService {
-  constructor(@InjectRepository(Wishlist) private wishlistRepository: Repository<Wishlist>) {}
+  constructor(
+    @InjectRepository(Wishlist) private wishlistRepository: Repository<Wishlist>,
+    private readonly usersService: UsersService
+    ) {}
 
-  create(createWishlistDto: CreateWishlistDto) {
-    return this.wishlistRepository.save(createWishlistDto);
+  async create(userId: number, createWishlistDto: CreateWishlistDto) {
+    const owner = await this.usersService.findOne({ where: { id: userId }});
+    return this.wishlistRepository.save({...createWishlistDto, owner });
   }
 
   findMany(query?: FindManyOptions<Wishlist>) {
