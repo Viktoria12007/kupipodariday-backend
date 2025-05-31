a9b60c5dcc3cec97e80016a4caa9933593b97ee3
Виктория Голубова <v.golubova@denexy.com>
2025-05-31 19:38:15 +0600

    'Create' method has been supplemented for offers.service.ts

--- src/offers/offers.service.ts
+++ src/offers/offers.service.ts
@@ -1,16 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { CreateOfferDto } from './dto/create-offer.dto';
 import { InjectRepository } from "@nestjs/typeorm";
 import { FindManyOptions, FindOneOptions, Repository } from "typeorm";
 import { Offer } from "./entities/offer.entity";
+import { UsersService } from "../users/users.service";
+import { WishesService } from "../wishes/wishes.service";
 
 @Injectable()
 export class OffersService {
-  constructor(@InjectRepository(Offer) private offerRepository: Repository<Offer>) {}
+  constructor(
+    @InjectRepository(Offer) private offerRepository: Repository<Offer>,
+    private readonly usersService: UsersService,
+    private readonly wishesService: WishesService,
+  ) {}
 
-  async create(createOfferDto: CreateOfferDto) {
-    const offer = await this.offerRepository.create(createOfferDto);
-    return this.offerRepository.save(offer);
+  async create(userId: number, createOfferDto: CreateOfferDto) {
+    const { amount, itemId } = createOfferDto;
+    const user = await this.usersService.findOne({ where: { id: userId }});
+    const item = await this.wishesService.findOne({ where: { id: itemId }});
+
+    const raised = item.raised + amount;
+    if (raised > item.price) {
+      throw new BadRequestException('Сумма заявки больше чем осталось собрать');
+    }
+    item.raised += amount;
+
+    await this.wishesService.updateOne({ id: itemId }, { raised });
+
+    return this.offerRepository.save({ ...createOfferDto, user, item });
   }
 
   findMany(query?: FindManyOptions<Offer>) {
