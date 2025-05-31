35beb2724518c56da7056c07c862f6010b757894
Виктория Голубова <v.golubova@denexy.com>
2025-05-31 19:15:24 +0600

    Used modules added to imports.

--- src/wishlists/wishlists.module.ts
+++ src/wishlists/wishlists.module.ts
@@ -3,9 +3,10 @@ import { WishlistsService } from './wishlists.service';
 import { WishlistsController } from './wishlists.controller';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Wishlist } from "./entities/wishlist.entity";
+import { UsersModule } from "../users/users.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Wishlist])],
+  imports: [TypeOrmModule.forFeature([Wishlist]), UsersModule],
   controllers: [WishlistsController],
   providers: [WishlistsService],
   exports: [WishlistsService]
