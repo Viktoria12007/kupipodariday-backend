dc44bf215fcadef67f4cb582192361655fa4d421
Виктория Голубова <v.golubova@denexy.com>
2025-07-01 10:35:58 +0600

    Added import 'WishesModule'.

--- src/wishlists/wishlists.module.ts
+++ src/wishlists/wishlists.module.ts
@@ -4,9 +4,10 @@ import { WishlistsController } from './wishlists.controller';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Wishlist } from "./entities/wishlist.entity";
 import { UsersModule } from "../users/users.module";
+import { WishesModule } from "../wishes/wishes.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Wishlist]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Wishlist]), UsersModule, WishesModule],
   controllers: [WishlistsController],
   providers: [WishlistsService],
   exports: [WishlistsService]
